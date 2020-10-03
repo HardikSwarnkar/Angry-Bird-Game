c233dbd808c796ed3423f1d566f1eef32e29f4fc
HardikSwarnkar <72317454+HardikSwarnkar@users.noreply.github.com>
2020-10-04 00:40:55 +0530

    commit
    unity mini project for github

---
+++ angry bird/Assets/Die.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Die : MonoBehaviour
+{
+    public GameObject Bird;
+    public GameObject Pattern;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "ground")
+        {
+            Destroy();
+        }
+    }
+    public void Destroy()
+    {
+        Destroy(Bird);
+        Instantiate(Pattern, transform.position, Quaternion.identity);
+    }
+}
